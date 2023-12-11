383df2e02ec59289d5b729d3a14d3728ed7f4c6c
hussaiinn <hlokhandwala21@gmail.com>
2023-12-11 18:56:41 +0530

    made change to next.config.json

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
-
-module.exports = {
+const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = {
       "mongodb-client-encryption": false,
       aws4: false,
+      // Add other fallbacks if needed
     };
+    if (!isServer) {
+      config.experiments = { webpackBuildWorker: true };
+    }
 
     return config;
   },
 };
+
+module.exports = nextConfig;
